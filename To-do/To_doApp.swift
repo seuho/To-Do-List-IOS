79093743acd12d5298ee595e6b121657f5e30fe6
yashasvi <yashasvipamu.ln352@gmail.com>
2024-11-10 10:07:41 -0600

    Initial Commit

---
+++ To-do/To_doApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_doApp.swift
+//  To-do
+//
+//  Created by Yashasvi Pamu on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct To_doApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
